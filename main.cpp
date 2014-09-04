c3729c3c0fbdf6facead0eaaefc3d7d7cafe3a7c
William Yager <will.yager@gmail.com>
2014-09-04 00:32:46 -0500

    Cleaned up

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 //  main.cpp
 //  Teensy LED strip controller
 //
-//  Created by Will Yager on 6/25/13.
-//  Copyright (c) 2013 Will Yager. All rights reserved.
+//  Created by Will Yager on 2014-09-01.
+//  Copyright (c) 2014 Will Yager. All rights reserved.
 //
 
 extern "C"{
@@ -12,16 +12,12 @@ extern "C"{
 #include "usb_serial.h"
 #include "LPD8806/LPD8806.h"
 }
-#define LED_CONFIG	(DDRD |= (1<<6))
-#define LED_ON		(PORTD |= (1<<6))
-#define LED_OFF		(PORTD &= ~(1<<6))
-#define LED_TOGGLE	(PORTD ^= (1<<6)) 
+
 #define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
 
-void parse_and_execute_command(const char *buf, uint8_t num);
-void send_nibble_as_hex(const uint8_t nibble);
-void send_byte_as_hex(const uint8_t byte);
-
+void wait_for_byte(){
+	while(!usb_serial_available()){/* wait */};
+}
 
 int main(void)
 {
@@ -37,13 +33,14 @@ int main(void)
 	usb_serial_flush_input();
 
 	while (1) {
-		
+		// Repeatedly read RGB data over USB serial and then
+		// forward it to the LED strip
 		for(uint8_t pixel = 0; pixel < 32; pixel++){
-			while(!usb_serial_available()){};
+			wait_for_byte();
 			uint8_t r = usb_serial_getchar();
-			while(!usb_serial_available()){};
+			wait_for_byte();
 			uint8_t g = usb_serial_getchar();
-			while(!usb_serial_available()){};
+			wait_for_byte();
 			uint8_t b = usb_serial_getchar();
 			strips[0].pixels[pixel] = {r, g, b};
 		}
@@ -51,24 +48,3 @@ int main(void)
 		LPD8806_send(strips, num_strips);
 	}
 }
-
-
-//Send a byte as an unsigned hex value
-void send_byte_as_hex(const uint8_t byte){
-	send_nibble_as_hex(byte >> 4);
-	send_nibble_as_hex(byte & 0x0F);
-}
-//Send a nibble as a hex digit to the USB serial device
-//Invalid values are represented as a '#'
-void send_nibble_as_hex(const uint8_t nibble){
-	if(nibble>15){
-		usb_serial_putchar('#');
-	}
-	else if(nibble<10){
-		usb_serial_putchar(nibble + 48);
-	}
-	else{
-		usb_serial_putchar(nibble + 55);
-	}
-}
-
