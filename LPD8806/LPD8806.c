03ac8aa9bb0803330012d5f8e874fb7cf0147f49
William Yager <will.yager@gmail.com>
2014-09-03 21:43:04 -0500

    Revert 5064680..dfcf249
    This rolls back to commit 5064680f532951b624623a5056f5d9d3fa8246a4.

--- LPD8806/LPD8806.c
+++ LPD8806/LPD8806.c
@@ -1,52 +1,43 @@
 #include "LPD8806.h"
 #include <avr/io.h>
-#include <util/delay.h>
 
-#define CLOCKPIN 2
-#define DATAPIN 3
-inline void set_clock_high(void){ PORTB |= 1 << CLOCKPIN; _delay_ms(1);}
-inline void set_clock_low(void){ PORTB &= ~(1 << CLOCKPIN); _delay_ms(1);}
-inline void set_data_high(void){ PORTF |= 1 << DATAPIN; _delay_ms(1);}
-inline void set_data_low(void){ PORTF &= ~(1 << DATAPIN); _delay_ms(1);}
-inline void set_data_out(void){ DDRF |= 1 << DATAPIN; }
-inline void set_clock_out(void){ DDRB |= 1 << CLOCKPIN; }
+#define CLOCKPIN 4
+#define DATAPIN 5
+inline void set_clock_high(void){ PORTD |= 1 << CLOCKPIN; }
+inline void set_clock_low(void){ PORTD &= ~(1 << CLOCKPIN); }
+inline void set_data_high(void){ PORTD |= 1 << DATAPIN; }
+inline void set_data_low(void){ PORTD &= ~(1 << DATAPIN); }
+inline void set_data_out(void){ DDRD |= 1 << DATAPIN; }
+inline void set_clock_out(void){ DDRD |= 1 << CLOCKPIN; }
 void clock_strobe(void)  {set_clock_high(); set_clock_low();}
 // Prepare teensy pins and then prepare the strip
-void LPD8806_IO_init(uint8_t num_strips){
+void LPD8806_IO_init(size_t num_strips){
   set_data_out();
   set_clock_out();
-  for (uint8_t i = 0; i < num_strips; i++){
+  for (size_t i = 0; i < num_strips; i++){
     LPD8806_send_byte(0);
   }
 }
 
-void LPD8806_send_bit(uint8_t bit){
-  if(bit == 0){
-    set_data_low();
-  }
-  else {
-    set_data_high();
-  }
-  clock_strobe();
-}
 void LPD8806_send_byte(uint8_t the_byte){
-  for(uint8_t mask=0x80; mask; mask >>= 1) {
-    uint8_t bit = the_byte & mask;
+  for(uint8_t i = 0; i < 8; i++){
+    uint8_t bit = the_byte & (1 << (7 - i));
     if(bit == 0) set_data_low();
     else set_data_high();
     clock_strobe();
   }
 }
-void LPD8806_send(strip_data* strips, uint8_t num_strips){
-  for(uint8_t i = 0; i < num_strips; i++){
-    for(uint8_t px = 0; px < 32; px++){
-      color pixel = strips[i].pixels[px];
+void LPD8806_send(strip_data* strips, size_t num_strips){
+  for(size_t i = 0; i < num_strips; i++){
+    strip_data* strip = &strips[i];
+    for(size_t px = 0; px < 32; px++){
+      color pixel = strip->pixels[px];
       LPD8806_send_byte(pixel.g | 0x80);
       LPD8806_send_byte(pixel.b | 0x80);
       LPD8806_send_byte(pixel.r | 0x80);
     }
   }
-  for(uint8_t i = 0; i < num_strips; i++){
+  for(size_t i = 0; i < num_strips; i++){
     LPD8806_send_byte(0);
   }
 }
