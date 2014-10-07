4ef8577e2407e9d84ba5d003e814f3c556964b55
William Yager <will.yager@gmail.com>
2014-10-07 15:17:11 -0500

    new constant

--- led_driver.py
+++ led_driver.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
 
 	colors = normalize_colors(generate_colors(32))
 	
-	colors = multiply_colors(colors, notes, scalar = 1.1)
+	colors = multiply_colors(colors, notes, scalar = 85.1)
 
 	colors = cap_colors(colors, cap = 127.0)
 
